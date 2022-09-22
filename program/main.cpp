628b62eda808adf95041f6dd53d0a4756dd003cc
As Project <wafuumisoshiru@gmail.com>
2022-09-23 01:07:47 +0900

    Update main.cpp

--- program/main.cpp
+++ program/main.cpp
@@ -1,5 +1,26 @@
 #include <fstream>
+#include <iomanip>
+#include <string>
+#include <ctime>
+
+std::string getDate() {
+    std::time_t t = std::time(nullptr);
+    const std::tm* localTime = std::localtime(&t);
+    std::stringstream s;
+    s << localTime->tm_year + 1900 << "/";
+    s << std::setw(2) << std::setfill('0') << localTime->tm_mon + 1 << "/";
+    s << std::setw(2) << std::setfill('0') << localTime->tm_mday << " ";
+    s << std::setw(2) << std::setfill('0') << localTime->tm_hour << ":";
+    s << std::setw(2) << std::setfill('0') << localTime->tm_min << ":";
+    s << std::setw(2) << std::setfill('0') << localTime->tm_sec;
+    return s.str();
+}
+
 int main() {
   std::ofstream ofs("../data/test.tsv");
   ofs << "Test\tA\tB\n1\tA1\tB1\n2\tA2\tB2";
+  
+  std::ofstream ofs2("../data/README.md");
+  ofs2 << "# Update Date\n";
+  ofs2 << getDate();
 }
